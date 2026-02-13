cfd3fe82b46f9c4bd9ad42c68528193694ce3916
Abhay433 <abhayn433@gmail.com>
2026-02-13 12:32:49 +0530

    fixed UI

--- src/app/components/howwework/howwework.component.ts
+++ src/app/components/howwework/howwework.component.ts
@@ -6,6 +6,7 @@ import { SupabaseService } from '../../services/supabase.service';
 
 @Component({
   selector: 'app-howwework',
+  standalone: true,
   imports: [CommonModule,FormsModule],
   templateUrl: './howwework.component.html',
   styleUrl: './howwework.component.css'
@@ -22,10 +23,23 @@ export class HowweworkComponent {
     message: ''
   };
 
+  private toTitleCase(str: string): string {
+    if (!str) return '';
+    return str.toLowerCase().split(' ').map(word => 
+      word.charAt(0).toUpperCase() + word.slice(1)
+    ).join(' ');
+  }
+
   async onSubmit(contactForm: any) {
+
+    // 1. Data Clean-up (Capitalize Names & Company)
+    this.form.full_name = this.toTitleCase(this.form.full_name);
+    this.form.company = this.toTitleCase(this.form.company);
+    this.form.email = this.form.email.toLowerCase().trim(); // Email hamesha lowercase
+
     console.log('Form submitting:', this.form);
 
-    const { data, error } = await this.supabaseService.saveContact(this.form);
+    const { error } = await this.supabaseService.saveContact(this.form);
 
     if (error) {
       console.error('Database error:', error.message);
